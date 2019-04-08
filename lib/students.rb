c1e2ec8c28bc984d1d6c4da9bfeb2d90964fd4c0
brenden williams <brendenw7@gmail.com>
2019-04-08 17:29:15 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,7 +13,7 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  "select count(student.tardies) from students"
+  "select count(tardies) from students"
 end
 
 def average_gpa_for_9th_grade
